5ef1ba4879bb9f39d950723deb9214c57a046399
Kelsea McAllister <kelsea.mcall@gmail.com>
2020-09-17 16:05:23 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,9 @@
 def position_taken? (board, index)
   if board[index] == " " or "" || nil
     false
-  elsif board[index] == "X"
+  elsif board[index] == X
     true
-  elsif board[index] == "O"
+  elsif board[index] == O
     true
   end
 end
